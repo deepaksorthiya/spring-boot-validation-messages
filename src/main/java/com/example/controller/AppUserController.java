1d4f569ddc795aaaf278cef217d1197bf0e1072a
Deepak Katariya <deepak.sorthiya@gmail.com>
2023-11-17 18:29:16 +0530

    fix imports

--- src/main/java/com/example/controller/AppUserController.java
+++ src/main/java/com/example/controller/AppUserController.java
@@ -7,7 +7,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -28,18 +27,17 @@ public class AppUserController {
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<AppUser> save(@RequestBody @Validated AppUser appUser, @RequestHeader HttpHeaders httpHeaders) {
+    public ResponseEntity<AppUser> save(@RequestBody @Validated AppUser appUser) {
         return new ResponseEntity<>(appUserRepository.save(appUser), HttpStatus.CREATED);
     }
 
     @GetMapping(value = "{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<AppUser> getUser(@PathVariable long userId, @RequestHeader HttpHeaders httpHeaders) {
+    public ResponseEntity<AppUser> getUser(@PathVariable long userId) {
         return new ResponseEntity<>(appUserRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found.")), HttpStatus.OK);
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Page<AppUser>> findAll(@NotNull final Pageable pageable,
-                                                 @RequestHeader HttpHeaders httpHeaders) {
+    public ResponseEntity<Page<AppUser>> findAll(@NotNull final Pageable pageable) {
         System.out.println(entityManager);
         Map<String, Object> properties = entityManager.getProperties();
         System.out.println(properties);
