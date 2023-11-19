dc84c0506a5bc2995fce1aca21bea60477db48c7
Deepak Katariya <deepak.sorthiya@gmail.com>
2023-11-19 13:26:41 +0530

    validator beans check

--- src/main/java/com/example/SpringCommandLineRunner.java
+++ src/main/java/com/example/SpringCommandLineRunner.java
@@ -4,14 +4,17 @@ import com.example.model.AppUser;
 import com.example.repository.AppUserRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.validation.Validator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -22,12 +25,15 @@ public class SpringCommandLineRunner implements CommandLineRunner {
     private final AppUserRepository appUserRepository;
     private final List<RequestMappingInfoHandlerMapping> requestMappingInfoHandlerMapping;
 
+    private final ApplicationContext applicationContext;
+
     @PersistenceContext
     private EntityManager entityManager;
 
-    public SpringCommandLineRunner(AppUserRepository appUserRepository, List<RequestMappingInfoHandlerMapping> requestMappingInfoHandlerMapping) {
+    public SpringCommandLineRunner(AppUserRepository appUserRepository, List<RequestMappingInfoHandlerMapping> requestMappingInfoHandlerMapping, ApplicationContext applicationContext) {
         this.appUserRepository = appUserRepository;
         this.requestMappingInfoHandlerMapping = requestMappingInfoHandlerMapping;
+        this.applicationContext = applicationContext;
     }
 
     @Override
@@ -40,6 +46,12 @@ public class SpringCommandLineRunner implements CommandLineRunner {
         for (Map.Entry<RequestMappingInfo, HandlerMethod> entry : handlersMethod.entrySet()) {
             log.info(entry.getKey() + " : " + entry.getValue());
         }
+
+        String[] names1 = applicationContext.getBeanNamesForType(Validator.class);
+        String[] names2 = applicationContext.getBeanNamesForType(org.springframework.validation.Validator.class);
+
+        log.info("Jakarta Validator Beans : {} ", Arrays.toString(names1));
+        log.info("Spring Validator Beans : {} ", Arrays.toString(names2));
 
         List<AppUser> appUserList = new ArrayList<>();
 
